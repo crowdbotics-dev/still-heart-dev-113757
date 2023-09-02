30337cb768db1abca1ee03ccc67d8524f0c7cf8a
Crowdbotics <engineering@crowdbotics.com>
2023-09-02 23:22:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-heart-dev-113757",
+  url: "https://still-heart-dev-113757.botics.co",
+  api: "https://still-heart-dev-113757.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
